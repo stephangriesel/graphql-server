cc6b138b49737d0e6e18258edb13fcf695871864
Stephan Griesel <sgriesel@gmail.com>
2018-11-09 06:32:32 +0100

    fix typo in server.js

--- schema.js
+++ schema.js
@@ -7,6 +7,45 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
-module.exports = new GraphQLSchema({
+// Data
+const customers = [
+    {id:'1', name:'John Doe', email:'jdoe@gmail.com', age:35},
+    {id:'2', name:'Frikkie Bas', email:'frik@gmail.com', age:15},
+    {id:'3', name:'Koos Wortel', email:'wortelluva@gmail.com', age:62},
+];
 
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+    name: 'Customer',
+    fields:() => ({
+        id: {type:GraphQLString},
+        name: {type:GraphQLString},
+        email: {type:GraphQLString},
+        age: {type:GraphQLInt},
+    })
+});
+
+// Root query
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:{
+        customer: {
+            type:CustomerType,
+            args:{
+                id:{type:GraphQLString}
+            },
+            resolve(parentValue, args){
+                for(let i = 0;i < customers.length;i++){
+                    if(customer[i].id == args.id){
+                        return customers[i];
+                    }
+                }
+            }
+        }
+    }
+    
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
-const schema = require.apply('./schema.js');
+const schema = require('./schema.js');
 
 const app = express();
 
-app.use('graphql', expressGraphQL({
+app.use('/graphql', expressGraphQL({
     schema:schema,
     graphiql:true
 }));
